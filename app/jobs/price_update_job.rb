b33637d0a4204ac7db18ecd4e7dd28c929613be6
dbroemme <darren.broemmer@gmail.com>
2022-07-20 12:08:36 -0400

    Schedule sidekiq job and have it check current price

--- app/jobs/price_update_job.rb
+++ app/jobs/price_update_job.rb
@@ -3,5 +3,19 @@ class PriceUpdateJob < ApplicationJob
 
   def perform(*args)
     puts "Running the price update job"
+
+    most_recent_day = CryptoData.maximum(:day)
+    puts "The most recent day in the database is #{most_recent_day}"
+
+    # check if this is today
+    now = Time.now
+    today_str = now.strftime("%Y-%m-%d")
+    if today_str == most_recent_day
+      puts "We are up to date!"
+    else 
+      puts "We are missing data for today #{today_str}"
+      current_btc_price = CryptoCommon::get_current_btc_in_usc
+      puts "The current price as of #{Time.now} is #{current_btc_price}"
+    end
   end
 end
